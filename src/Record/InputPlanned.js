156a01d0b799622962b919cda06abebab3629038
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-01-25 11:31:25 -0800

    Set-up stateful and stateless example

---
+++ src/Record/InputPlanned.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecordInputPlanned = props => {
+  return (
+    <div>
+      <p>Enter planned savings:</p>
+      <input type="text"></input>
+    </div>
+  );
+};
+
+export default RecordInputPlanned;
---
+++ src/Record/InputSpent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecordInputSpent = props => {
+  return (
+    <div>
+      <p>Enter spent:</p>
+      <input type="text"></input>
+    </div>
+  );
+};
+
+export default RecordInputSpent;
