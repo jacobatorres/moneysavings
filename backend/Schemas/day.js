b7caee7da69f9831e375121339b3e4307921d342
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-02-15 19:23:30 -0800

    modify date schema to add label

---
+++ backend/Schemas/day.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const daySchema = new mongoose.Schema({
+  bill_value: Number,
+  bill_label: String,
+  food: Number,
+  food_label: String,
+  tr_value: Number,
+  tr_label: String,
+  leisure_value: Number,
+  leisure_label: String,
+  timestamp: Date
+});
+
+console.log('This is at Day Schema');
+
+module.exports = mongoose.model('Day', daySchema);
