26f093e0a0bf0ab67ffdbedeec27ba7620f8644b
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-02-15 22:13:19 -0800

    Save label in FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,14 @@ import RecordInputSpent from './Record/InputSpent';
 class App extends Component {
   state = {
     bill_value: 0,
+    bill_label: 'bill label',
     food_value: 0,
+    food_label: 'food label',
     tr_value: 0,
-    leisure_value: 0
+    tr_label: 'transportation label',
+    leisure_value: 0,
+    leisure_label: 'leisure label',
+    timestamp: Date.now()
   };
 
   valuePlannedChangedBill = event => {
@@ -29,6 +34,19 @@ class App extends Component {
     this.setState({ leisure_value: event.target.value });
   };
 
+  labelPlannedChangedBill = event => {
+    this.setState({ bill_label: event.target.value });
+  };
+  labelPlannedChangedFood = event => {
+    this.setState({ food_label: event.target.value });
+  };
+  labelPlannedChangedTr = event => {
+    this.setState({ tr_label: event.target.value });
+  };
+  labelPlannedChangedleisure = event => {
+    this.setState({ leisure_label: event.target.value });
+  };
+
   saveRecordtoDB = event => {
     event.preventDefault();
     console.log('i made it here');
@@ -41,7 +59,12 @@ class App extends Component {
         bill_value: this.state.bill_value,
         food_value: this.state.food_value,
         tr_value: this.state.tr_value,
-        leisure_value: this.state.leisure_value
+        leisure_value: this.state.leisure_value,
+
+        bill_label: this.state.bill_value,
+        food_label: this.state.food_value,
+        tr_label: this.state.tr_value,
+        leisure_label: this.state.leisure_value
       })
       .then(response => {
         console.log('tumama i guess');
@@ -80,14 +103,31 @@ class App extends Component {
             value={this.state.bill_value}
           />
           <RecordInputSpent
+            label="Bills Label"
+            changed={this.labelPlannedChangedBill}
+            value={this.state.bill_label}
+          />
+
+          <RecordInputSpent
             label="Food"
             changed={this.valuePlannedChangedFood}
             value={this.state.food_value}
           />
           <RecordInputSpent
+            label="Food Label"
+            changed={this.labelPlannedChangedFood}
+            value={this.state.food_label}
+          />
+
+          <RecordInputSpent
             label="Transportation"
             changed={this.valuePlannedChangedTr}
             value={this.state.tr_value}
+          />
+          <RecordInputSpent
+            label="Transportation Label"
+            changed={this.labelPlannedChangedTr}
+            value={this.state.tr_label}
           />
 
           <RecordInputSpent
@@ -95,6 +135,12 @@ class App extends Component {
             changed={this.valuePlannedChangedLeisure}
             value={this.state.leisure_value}
           />
+          <RecordInputSpent
+            label="Leisure Label"
+            changed={this.valuePlannedChangedBill}
+            value={this.state.leisure_label}
+          />
+
           {/* <p className={color_result}>
             Result: <strong>{result_saved}</strong>
           </p> */}
