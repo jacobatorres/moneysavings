6942e8b4931e1d7cdb060ac0b4184c0a06bdadfe
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-03-07 11:33:42 -0800

    Style the hamburger menu

---
+++ frontend/src/components/SideDrawer/DrawerToggleButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './DrawerToggleButton.css';
+
+const drawerToggleButton = props => (
+  <button className="togglebutton">
+    <div className="togglebutton-line"></div>
+    <div className="togglebutton-line"></div>
+    <div className="togglebutton-line"></div>
+  </button>
+);
+
+export default drawerToggleButton;
---
+++ frontend/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import './Toolbar.css';
+
+const toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar_nav">
+      <div>
+        <DrawerToggleButton />
+      </div>
+      <div className="toolbar_logo">
+        <a href="/">LOGO</a>
+      </div>
+      <div className="space-right"></div>
+      <div className="toolbar_navitems">
+        <ul>
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <a href="/">Logout</a>
+          </li>
+          <li>
+            <a href="/">Logmiddle</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;
+// there are links, you can put in router links there
